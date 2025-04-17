da1802cbb4dc8b6f7132142ed4bc4026541f751a
bublik116 <bublik116@gmail.com>
2025-04-17 20:29:05 +0500

    Update def three parametrs.py
    добавил в цикл пропуск числа на 4 и окончание на 9, вывод сообщения о выводе числа квадрата больше 50, добавил вызов второй функции с другим значением

--- def three parametrs.py
+++ def three parametrs.py
@@ -5,11 +5,21 @@ process(car = "aveo", drive = True, speed = 140.9)
 def process(number):
     number1 = int(number)
     num = 0
-    for num in range (number1):
+    for num in range(number1):
+        num1 = (num*num)
+        num + 1
+        print(num1)
+        if num1 > 50:
+                print(f"Квадрат числа {num} больше  50")
         if num < number1:
-            num + 1
-            print(num*num)
+            if num == 4:
+                continue
+            if num == 9:
+                break
+            
+          
         else:
             break
 process(number = 10.0)
+process(number = 24.0)
 
