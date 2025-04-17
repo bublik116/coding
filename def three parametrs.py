2cff61e0a20ab9da32197d7036c6bfef3529a9a6
bublik116 <bublik116@gmail.com>
2025-04-17 19:55:06 +0500

    Update def three parametrs.py
    создал отдельную функцию на передачу числа 10.0 в аргумент функции, и преобразовал его и проитерировал  затем сделал квадрат этого  числа

--- def three parametrs.py
+++ def three parametrs.py
@@ -1,3 +1,15 @@
 def process (car, drive, speed):
     pass
 process(car = "aveo", drive = True, speed = 140.9)
+
+def process(number):
+    number1 = int(number)
+    num = 0
+    for num in range (number1):
+        if num < number1:
+            num + 1
+            print(num*num)
+        else:
+            break
+process(number = 10.0)
+
