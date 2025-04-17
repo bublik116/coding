861f546d399b88fe790a10b07fe1ca7f710cd58d
bublik116 <bublik116@gmail.com>
2025-04-17 21:05:13 +0500

    Update def three parametrs.py
     В цикл добавил вывод сообщения "Квадрат числа {number} больше или равен 20 и меньше или равен 50".
    
    В цикл добавил вывод сообщения "Квадрат числа {number} меньше 20".
    
    3) В функцию добавил проверку значения str на пустоту и если оно пусто вывавать исключение valueError.

--- def three parametrs.py
+++ def three parametrs.py
@@ -1,25 +1,26 @@
-def process (car, drive, speed):
-    pass
-process(car = "aveo", drive = True, speed = 140.9)
-
 def process(number):
     number1 = int(number)
     num = 0
+    string1 = "proverka"
+    if string1 =="":
+        raise ValueError("Пустая строка")
+    
     for num in range(number1):
         num1 = (num*num)
         num + 1
         print(num1)
-        if num1 > 50:
-                print(f"Квадрат числа {num} больше  50")
         if num < number1:
             if num == 4:
                 continue
+            if num1 > 50:
+                print(f"Квадрат числа {num} больше 50")
+            if num1 < 20:
+                print(f"Квадрат числа {num} меньше 20")
+            if num1 >= 20 and  num1 <= 50:
+                print(f"Квадрат числа {num}  больше  или равен  20 и меньше или равен  50")
             if num == 9:
                 break
             
-          
-        else:
-            break
+                
 process(number = 10.0)
-process(number = 24.0)
 
