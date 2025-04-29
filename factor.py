9bfbfe3403122a4712e4c00cd85968402196e9e6
bublik116 <bublik116@gmail.com>
2025-04-29 19:45:16 +0500

    Update factor.py
    обработка ошибок

--- factor.py
+++ factor.py
@@ -1,10 +1,6 @@
-def factorial(n):
-    if n == 0:
-        return 1
-    else:
-        return n * factorial(n - 1)
-my_file = open("factor", "w+")
-my_file.write(str(factorial(11)))
-my_file.close()
-my_file = open("factor", "r")
-print(my_file.read())
+
+try:
+ with open("factor") as f:
+ print(f.read())
+except FileNotFoundError:
+ print("Файл не найден")
