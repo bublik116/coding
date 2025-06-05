32011dd29cb7ce3709fbf00c2f642583f8c14137
bublik116 <bublik116@gmail.com>
2025-06-05 19:26:51 +0500

    дополнил код
    В классе Student добавил метод display_info(self).
    
    Метод выводит строку: "Имя: <name>, ID: <student_id>".

--- education.py
+++ education.py
@@ -2,3 +2,5 @@ class Student:
     def __init__(self, name, student_id):
         self.name = name
         self.student_id = student_id
+    def display_info(self):
+        print(f"Имя: {self.name}, ID: {self.student_id}")
