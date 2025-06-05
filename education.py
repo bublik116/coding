8260104b980ba07c098c3bde7286b29620b97b7d
bublik116 <bublik116@gmail.com>
2025-06-05 20:21:25 +0500

    Update education.py
    добавил метод который добавляет объект student в список self.students.

--- education.py
+++ education.py
@@ -2,8 +2,14 @@ class Student:
     def __init__(self, name, student_id):
         self.name = name
         self.student_id = student_id
+    
     def display_info(self):
         print(f"Имя: {self.name}, ID: {self.student_id}")
+
+
 class Group:
-      def __init__(self, student):
-        self.students = [student]
+    def __init__(self, student):
+        self.students = [student]  # Создаём список с первым студентом
+    
+    def add_student(self, student):
+        self.students.append(student)  #  добавление нового студента
