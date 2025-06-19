0869534038d6ee6bc6a20bcc079d76f62fc3c51d
bublik116 <bublik116@gmail.com>
2025-06-19 20:18:09 +0500

    Update serializer.py
    1) В метод save добавьте код: https://github.com/str-top/python2025/discussions/220#discussioncomment-13496460
    
    2) В метод load добавьте код: https://github.com/str-top/python2025/discussions/220#discussioncomment-13496474
    
    3) Внимательно ознакомьтесь с добавленным кодом и разберитесь в его работе
    
    4) Исправьте ошибку связанную с переменной filename. Попробуйте сами вначале понять о чем речь.

--- serializer.py
+++ serializer.py
@@ -1,8 +1,6 @@
 import json
 import os
 
-Book_all = []
-
 class Book:
     def __init__(self, name, author, year):
         self.name = name
@@ -24,81 +22,82 @@ class Book:
         return cls(data["name"], data["author"], data["year"])
 
 class library:
+    def __init__(self):
+        self.books = []
+        self.load()  # Загружаем данные при создании экземпляра
+    
     def new_book(self, book):
-        Book_all.append(book)
+        self.books.append(book)
         print(f"Книга '{book.name}' добавлена в библиотеку.")
         self.save()
     
     def del_book(self, book_name):
-        for book in Book_all:
+        for book in self.books:
             if book.name == book_name:
-                Book_all.remove(book)
-                print(f"Книга '{book_name}' удалена из библиотеку.")
+                self.books.remove(book)
+                print(f"Книга '{book_name}' удалена из библиотеки.")
                 self.save()
                 return
         print(f"Книга '{book_name}' не найдена в библиотеке.")
     
     def show_book(self):
-        if not Book_all:
+        if not self.books:
             print("В библиотеке нет книг.")
         else:
             print("Список всех книг в библиотеке:")
-            for book in Book_all:
+            for book in self.books:
                 book.get_info()
     
-    def save(self):
+    def save(self, filename="library.json"):
         """Сохраняет все книги в файл"""
+        data = []
+        for book in self.books:
+            data.append(book.to_dict())
         try:
-            with open('library.json', 'w', encoding='utf-8') as f:
-                json.dump([book.to_dict() for book in Book_all], f, ensure_ascii=False, indent=4)
+            with open(filename, "w", encoding='utf-8') as f:
+                json.dump(data, f, ensure_ascii=False, indent=4)
         except Exception as e:
             print(f"Ошибка при сохранении: {e}")
     
-    def load(self):
+    def load(self, filename="library.json"):
         """Загружает книги из файла"""
         try:
-            if os.path.exists('library.json'):
-                with open('library.json', 'r', encoding='utf-8') as f:
+            if os.path.exists(filename):
+                with open(filename, "r", encoding='utf-8') as f:
                     data = json.load(f)
-                    Book_all.clear()
-                    for book_data in data:
-                        Book_all.append(Book.from_dict(book_data))
+                    self.books = [Book.from_dict(book_data) for book_data in data]
         except Exception as e:
             print(f"Ошибка при загрузке: {e}")
 
-# Инициализация библиотеки
-lib = library()
-lib.load()  # Загружаем данные при старте
-
 # Основной цикл программы
-while True:
-    print("\nВыберите действие:")
-    print("1 - Добавить книгу")
-    print("2 - Удалить книгу")
-    print("3 - Показать все книги")
-    
-    action = input("Ваш выбор (1-3): ")
-    
-    if action == "1":
-        print("\nДобавление новой книги:")
-        name = input("Название: ")
-        author = input("Автор: ")
-        year = input("Год издания: ")
-        lib.new_book(Book(name, author, year))
-    
-    elif action == "2":
-        if not Book_all:
-            print("\nВ библиотеке нет книг для удаления.")
+if __name__ == "__main__":
+    lib = library()
+
+    while True:
+        print("\nВыберите действие:")
+        print("1 - Добавить книгу")
+        print("2 - Удалить книгу")
+        print("3 - Показать все книги")
+        
+        action = input("Ваш выбор (1-3): ")
+        
+        if action == "1":
+            print("\nДобавление новой книги:")
+            name = input("Название: ")
+            author = input("Автор: ")
+            year = input("Год издания: ")
+            lib.new_book(Book(name, author, year))
+        
+        elif action == "2":
+            if not lib.books:
+                print("\nВ библиотеке нет книг для удаления.")
+            else:
+                print("\nУдаление книги:")
+                name = input("Введите название книги для удаления: ")
+                lib.del_book(name)
+        
+        elif action == "3":
+            lib.show_book()
+        
         else:
-            print("\nУдаление книги:")
-            name = input("Введите название книги для удаления: ")
-            lib.del_book(name)
-    
-    elif action == "3":
-        lib.show_book()
-    
-    else:
-        print("\nОшибка! Пожалуйста, введите 1, 2 или 3")
-    
-    else:
-        print("\nОшибка! Пожалуйста, введите 1, 2 или 3")
+            print("\nОшибка! Пожалуйста, введите 1, 2 или 3")
